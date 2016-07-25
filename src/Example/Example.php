2dffd9591ecc378389c1c6c1a5350adf8959463c
Jeff Puckett <jeff@jeffpuckett.com>
2016-07-24 22:19:25 -0500

    separate auto/manual tests and add dummy function

--- src/Example/Example.php
+++ src/Example/Example.php
@@ -2,13 +2,17 @@
 
 class Example
 {
-    public static function go($bool = false)
-    {
+    public static function go($bool = false){
         if ($bool) {
             return true;
         }
 
         return false;
-
     }
+
+    public static function echoVar($var){
+        $var += 1;
+        return $var;
+    }
+
 }
